ef50ec1d9eeccf586479cafb9ee58fbeb4ef49fc
Pao <pao.eight@gmail.com>
2017-11-23 14:36:09 -0600

    Add changes

--- ejercicios/4-canvas/dibujo.js
+++ ejercicios/4-canvas/dibujo.js
@@ -1,40 +1,49 @@
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var xi, yf;
 var colorcito = "#138D75";
 
+var texto = document.getElementById("numeroLineas");
+var boton = document.getElementById("botonMagico");
+boton.addEventListener("click", dibujarPorClick );
 
-for(l =0; l < lineas; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(colorcito, 0, yi, xf, 300);
+function dibujarPorClick() {
+
+  var lineas = parseInt(texto.value);
+  var espacio = ancho / lineas;
+  var l = 0;
+  var yi, xf;
+  var xi, yf;
+
+  for(l =0; l < lineas; l++) {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+  }
+
+  for(l =0; l < lineas; l++) {
+    xi = espacio * l;
+    yf = espacio * (l + 1);
+    dibujarLinea(colorcito, xi, 0, 300, yf);
+  }
+
+  for(l =0; l < lineas; l++) {
+    xi = espacio * l;
+    yf = espacio * (lineas - l);
+    dibujarLinea(colorcito, xi, 300, 300, yf);
+  }
+
+  for(l =0; l < lineas; l++) {
+    xi = espacio * l;
+    yf = espacio * (lineas - l);
+    dibujarLinea(colorcito, xi, 0, 0, yf);
+  }
+
+  dibujarLinea(colorcito, 1, 1, 1, 299);
+  dibujarLinea(colorcito, 1,299,299,299);
+  dibujarLinea(colorcito, 1, 1, 299, 1);
+  dibujarLinea(colorcito, 299, 1, 299, 299);
 }
-
-for(l =0; l < lineas; l++) {
-  xi = 10 * l;
-  yf = 10 * (l + 1);
-  dibujarLinea(colorcito, xi, 0, 300, yf);
-}
-
-for(l =0; l < lineas; l++) {
-  xi = 10 * l;
-  yf = 10 * (30 - l);
-  dibujarLinea(colorcito, xi, 300, 300, yf);
-}
-
-for(l =0; l < lineas; l++) {
-  xi = 10 * l;
-  yf = 10 * (30 - l);
-  dibujarLinea(colorcito, xi, 0, 0, yf);
-}
-
-dibujarLinea(colorcito, 1, 1, 1, 299);
-dibujarLinea(colorcito, 1,299,299,299);
-dibujarLinea(colorcito, 1, 1, 299, 1);
-dibujarLinea(colorcito, 299, 1, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
