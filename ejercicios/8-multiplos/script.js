32f0fb64b39ed3ba6dae1f1b0bf23ef5b0fa4e41
Pao <pao.eight@gmail.com>
2017-12-07 18:57:05 -0600

    Add new exercices

---
+++ ejercicios/8-multiplos/script.js
@@ -0,0 +1,18 @@
+var numero = document.getElementById("numeroUs");
+var boton = document.getElementById("botonSumar");
+boton.addEventListener("click", sumarMultiplos);
+
+function sumarMultiplos() {
+
+  var numero = parseInt(numeroUs.value);
+  var suma = 0;
+  for (var i = 1; i < numero; i++) {
+
+    var residuoTres = (i % 3);
+    var residuoCinco = (i % 5);
+    if (residuoTres == 0 || residuoCinco == 0) {
+     suma = (suma + i);
+    }
+  }
+  document.write("La suma de los múltiplos de 3 y 5 menores que " + numero + " es " + suma + "." + "<br/>");
+}
