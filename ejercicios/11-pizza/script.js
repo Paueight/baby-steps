ac5a2fabb8898544f921569670bc2466be36e2e4
Pao <pao.eight@gmail.com>
2017-12-13 16:46:41 -0600

    Add new changes

--- ejercicios/11-pizza/script.js
+++ ejercicios/11-pizza/script.js
@@ -1,5 +1,6 @@
 var personas = document.getElementById("personasUs");
 var pizzas = document.getElementById("pizzasUs");
+var rebanadas = document.getElementById("rebanadasUs");
 var boton = document.getElementById("botonPizza");
 boton.addEventListener("click", calcularRebanadas);
 
@@ -7,10 +8,10 @@ function calcularRebanadas() {
 
   var personas = parseInt(personasUs.value);
   var pizzas = parseInt(pizzasUs.value);
-  var rebanadas = (personas / pizzas);
-  var rebanadasDos = (rebanadas * pizzas);
-  var rebanadasTotal = (rebanadasDos / personas);
+  var rebanadas = parseInt(rebanadasUs.value);
+  var rebanadasPizza = (pizzas * rebanadas);
+  var rebanadasPersona = parseInt(rebanadasPizza / personas);
+  var pizzaSobra = (rebanadasPizza - (rebanadasPersona * personas));
 
-
-  document.write(personas + " personas con " + pizzas + " pizzas." + "<br/>" + "A cada persona le toca/n " + rebanadasTotal + " rebanada/s de pizza" + "<br/>" + "No sobra ninguna rebanada");
+  document.write(personas + " personas con " + pizzas + " pizzas." + "<br/>" + "A cada persona le toca/n " + rebanadasPersona + " rebanada/s de pizza" + "<br/>" + "Sobra/n " + pizzaSobra + " pizza/s.");
 }
